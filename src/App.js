f2c3612ff7e83e7386f57ae187896b9725a52713
Ataru Nakano <atarunakano@gmail.com>
2020-01-26 15:29:38 +0900

    learn jsx syntac

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 
 function App() {
   return (
-    <h1>
-      Hello! world
-    </h1>
+    <React.Fragment>
+      <h1>Hello! world</h1>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onClick={() => { console.log("I am clicked.") }}></input>
+    </React.Fragment>
   );
 }
 
