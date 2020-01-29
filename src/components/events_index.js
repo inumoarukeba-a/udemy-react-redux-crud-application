af64b27ac3ebe4e172a3fd50be8a9db0a16fcd52
Ataru Nakano <atarunakano@gmail.com>
2020-01-29 09:28:47 +0900

    implement delete feature

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
 import { Link } from "react-router-dom";
-import _ from 'lodash';
 
-import { readEvents } from '../actions';
+import { readEvents } from "../actions";
 
 class EventsIndex extends Component {
   componentDidMount() {
-    this.props.readEvents()
+    this.props.readEvents();
   }
+
   renderEvents() {
     return _.map(this.props.events, event => (
       <tr key={event.id}>
         <td>{event.id}</td>
-        <td>{event.title}</td>
+        <td>
+          <Link to={`/events/${event.id}`}>{event.title}</Link>
+        </td>
         <td>{event.body}</td>
       </tr>
-    ))
+    ));
   }
+
   render() {
     return (
       <React.Fragment>
@@ -29,17 +33,18 @@ class EventsIndex extends Component {
               <th>Body</th>
             </tr>
           </thead>
-          <tbody>
-            {this.renderEvents()}
-          </tbody>
+
+          <tbody>{this.renderEvents()}</tbody>
         </table>
+
         <Link to="/events/new">New Event</Link>
       </React.Fragment>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => ({ events: state.events })
-const mapDispatchToProps = ({ readEvents })
+const mapStateToProps = state => ({ events: state.events });
 
-export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
+const mapDispatchToProps = { readEvents };
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex);
